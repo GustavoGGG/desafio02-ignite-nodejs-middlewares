c620847df448c6c396466507cb84222da7e1b657
Gustavo Gomes <gu.gomes.89@gmail.com>
2022-01-20 23:35:27 -0300

    Finalizando desafio02 -Trabalhando Middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,56 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const userAlreadyExists = users.find((user) => user.username === username);
+  if (!userAlreadyExists) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+  request.user = userAlreadyExists
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const countTodo = user.todos.length;
+  if ((!user.pro) & (countTodo >= 10)) {
+    return response.status(403).json()
+  }
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if (!username) {
+    return response.status(404).json({ error: 'Username not information' })
+  }
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'Id UUId invalid' })
+  }
+  const userExists = users.find((user) => user.username === username);
+  if (!userExists) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+  const todoExists = userExists.todos.find((item) => item.id === id)
+  if (!todoExists) {
+    return response.status(404).json({ error: 'Todo not found' })
+  }
+  request.user = userExists
+  request.todo = todoExists
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const userAlreadyExists = users.find((user) => user.id === id);
+  if (!userAlreadyExists) {
+    return response.status(404).json({ error: 'User not found' })
+  }
+  request.user = userAlreadyExists
+  return next();
 }
 
 app.post('/users', (request, response) => {
